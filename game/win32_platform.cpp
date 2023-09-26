883800f82043f5a8e9f24d7f722d909f2db5a437
joao pedro <joao.pedro.souza.lopes2016@gmail.com>
2023-09-26 17:23:17 -0300

    window creating

---
+++ game/win32_platform.cpp
@@ -0,0 +1,24 @@
+#include <Windows.h>
+#include <tchar.h>
+
+bool running = true;
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nShowCmd) {
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = "GameWindowClass";
+	window_class.lpfnWndProc = window_callback;
+	//registerclass
+	RegisterClass(&window_class);
+	//creating window
+	HWND window = CreateWindow("GameWindowClass", "Pong - Tutorial", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	while (running) {
+		//input 
+		//sim
+
+	};
+};
