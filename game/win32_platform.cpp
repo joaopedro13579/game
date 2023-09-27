8ae0fcb63349ce819fad8caf23b4470ff8ce2111
joao pedro <joao.pedro.souza.lopes2016@gmail.com>
2023-09-27 13:26:24 -0300

    window already has response and can be moved and resized

--- game/win32_platform.cpp
+++ game/win32_platform.cpp
@@ -1,24 +1,48 @@
 #include <Windows.h>
-#include <tchar.h>
+
 
 bool running = true;
 
-LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
+{
+	LRESULT result = 0;
+
+	switch (uMsg)
+	{
+		case WM_CLOSE:
+		case WM_DESTROY:
+		{
+			running = false;
+		} break;
+		default:
+		{
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+		   return result;
 }
 
-int WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nShowCmd) {
+
+int WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nShowCmd) 
+{
 	WNDCLASS window_class = {};
 	window_class.style = CS_HREDRAW | CS_VREDRAW;
 	window_class.lpszClassName = "GameWindowClass";
 	window_class.lpfnWndProc = window_callback;
-	//registerclass
+//registerclass
 	RegisterClass(&window_class);
-	//creating window
-	HWND window = CreateWindow("GameWindowClass", "Pong - Tutorial", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
-	while (running) {
-		//input 
-		//sim
-
+//creating window
+	HWND window = CreateWindow("GameWindowClass", "Game_Name", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	while (running) 
+	{
+	//input 
+		MSG message;
+		while (PeekMessage(&message,window,0,0,PM_REMOVE))
+		{
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+	//sim
+	//render
 	};
 };
